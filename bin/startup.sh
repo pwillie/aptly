a603f1b8234d918509087c3509f5ab9d3f2fcb7c
Peter Wilson <pwillie@users.noreply.github.com>
2017-09-25 22:26:42 +1000

    handle passing path to private gpg key

--- bin/startup.sh
+++ bin/startup.sh
@@ -4,6 +4,8 @@ echo "Using:"
 echo -e "\tConfig file   : ${CONF_FILE:=/conf/aptly.conf}"
 echo -e "\tListen port   : ${PORT:=8000}"
 echo -e "\tCron schedule : ${CRON_SCHEDULE}"
+echo -e "\tPrivate key   : ${PRIVATE_KEY}"
+
 
 if [ ! -f "${CONF_FILE}" ]; then
     echo "Config file ${CONF_FILE} does not exit"
@@ -12,6 +14,10 @@ fi
 
 if [ -n "${CRON_SCHEDULE}" ]; then
     echo -e "${CRON_SCHEDULE} $(whoami) CONF_FILE=${CONF_FILE} /usr/local/bin/mirror.sh >> /proc/1/fd/1 2>&1\n" > /etc/cron.d/aptly_update_mirrors
+fi
+
+if [ -f "${PRIVATE_KEY}" ]; then
+    gpg --import $PRIVATE_KEY || true
 fi
 
 # TODO: setup S3PublishEndpoints
